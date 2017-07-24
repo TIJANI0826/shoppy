9ec16408df3741a0e8eb9aa6e0cf16bf4fa31c82
Tijani Ibrahim <ibrahimtijani08@gmail.com>
2017-07-24 20:25:53 +0100

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 admin.autodiscover()
-import products
+import products.views
 import carts
 import orders
 import account
